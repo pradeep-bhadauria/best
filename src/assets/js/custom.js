dfb9ea5fceb8ec4078fa048ce96b41ea37313c6a
Pradeep Bhadauria <pradeep.bhadauria08@gmail.com>
2018-08-02 21:08:13 +0530

    updatetd for ads

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -66,12 +66,13 @@ jQuery(document).ready(function() {
     },1000);
 });
 
+/*
 wow = new WOW({
     animateClass: 'animated',
     offset: 100
 });
 wow.init();
-
+*/
 jQuery(window).load(function() { // makes sure the whole site is loaded
     $('#status').fadeOut(); // will first fade out the loading animation
     $('#preloader').delay(100).fadeOut('slow'); // will fade out the white DIV that covers the website.
